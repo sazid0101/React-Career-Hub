f0be7db4b689330ea2f76eceb10a3b67356a526f
sazid0101 <shahriarislamsazid@gmail.com>
2025-04-19 11:57:21 +0600

    footer added

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 
 const Root = () => {
     return (
         <div>
-            <h2>this is root</h2>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,18 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './Components/Root/Root';
+import Home from './Components/Home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      }
+    ]
   },
 ]);
 

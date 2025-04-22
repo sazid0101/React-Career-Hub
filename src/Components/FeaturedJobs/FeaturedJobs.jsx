7ce4691f50eab9155dce39b71cfd73933ad4ce4b
sazid0101 <shahriarislamsazid@gmail.com>
2025-04-22 17:06:03 +0600

    handle Show All jobs

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -4,6 +4,8 @@ import Job from "../Job/Job";
 const FeaturedJobs = () => {
 
     const [jobs, setJobs] = useState([])
+    const [showAllData, setShowAllData] = useState(4)
+
     useEffect(()=>{
         fetch('jobs.json')
         .then(res => res.json())
@@ -14,11 +16,13 @@ const FeaturedJobs = () => {
             <h2 className="text-4xl font-bold text-center pb-5">Featured Jobs: {jobs.length}</h2>
             <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
 
-            <div className="grid grid-cols-2 gap-6 ">
+            <div className="grid grid-cols-2 gap-10 ">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, showAllData).map(job => <Job key={job.id} job={job}></Job>)
                 }
             </div>
+            <div className="mt-5 flex justify-center">
+                <button onClick={()=> setShowAllData(jobs.length)} className="btn btn-success text-white">Show All</button></div>
         </div>
     );
 };

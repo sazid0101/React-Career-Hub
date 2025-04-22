84628abd4362aae8ea42ebb47d5165d5788e9647
sazid0101 <shahriarislamsazid@gmail.com>
2025-04-22 12:39:31 +0600

    Set page layout and Display featured jobs

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -14,7 +14,7 @@ const FeaturedJobs = () => {
             <h2 className="text-4xl font-bold text-center pb-5">Featured Jobs: {jobs.length}</h2>
             <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
 
-            <div className="grid grid-cols-2 gap-2">
+            <div className="grid grid-cols-2 gap-6 ">
                 {
                     jobs.map(job => <Job key={job.id} job={job}></Job>)
                 }
--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { CiLocationOn } from "react-icons/ci";
+import { MdAttachMoney } from "react-icons/md";
 
-const Job = ({job}) => {
-    const {logo}= job
+const Job = ({ job }) => {
+    const { logo, job_title, company_name, remote_or_onsite, job_type, location, salary } = job
     return (
-        <div>
-            <img src={logo} alt="" srcset="" />
+        <div className="card bg-base-200 shadow-lg mt-10">
+            <figure>
+                <img className='w-40' src={logo} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{job_title}</h2>
+                <p>{company_name}</p>
+                <div className='font-bold'>
+                    <button className='border border-green-400 rounded mr-2 px-5 py-1'>{remote_or_onsite}</button>
+                    <button className='border border-green-400 rounded px-5 py-1'>{job_type}</button>
+                </div>
+
+                <div>
+                    <div className='flex items-center gap-1'>
+                        <CiLocationOn />
+                        <p>{location}</p>
+                    </div>
+                    <div className='flex items-center gap-1'>
+                        <MdAttachMoney />
+                        <p>{salary}</p>
+                    </div>
+                </div>
+                <div className="card-actions justify-center">
+                    <button className="btn btn-primary">View Details</button>
+                </div>
+            </div>
         </div>
     );
 };

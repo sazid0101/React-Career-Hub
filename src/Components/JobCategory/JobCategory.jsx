dc7f78f67fd2fd2a6166eef6b34c7d8a3cd2cc69
sazid0101 <shahriarislamsazid@gmail.com>
2025-04-25 11:53:43 +0600

    Job Category List

--- src/Components/JobCategory/JobCategory.jsx
+++ src/Components/JobCategory/JobCategory.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import SingleJobCategory from "../SingleJobCategory/SingleJobCategory";
+
 const JobCategory = () => {
+    const [jobsCategory,setJobsCategory] = useState([])
+
+    useEffect(()=>{
+        fetch('categories.json')
+        .then(Response => Response.json())
+        .then(data => setJobsCategory(data))
+    },[])
     return (
         <div>
             <h2 className="text-4xl font-bold text-center pb-5">Job Category List</h2>
             <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
+
+            <div className="grid grid-cols-4 mx-auto w-fit m-10">
+                {
+                    jobsCategory.map(job => <SingleJobCategory job={job} key ={job.id}></SingleJobCategory>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Components/SingleJobCategory/SingleJobCategory.jsx
@@ -0,0 +1,14 @@
+const SingleJobCategory = ({ job }) => {
+    const { logo, category_name, availability } = job
+    return (
+        <div className="ml-10 p-5 bg-teal-400 rounded-lg text-white">
+            <div className="flex justify-center">
+                <img src={logo} alt="" srcset="" />
+            </div>
+            <p className="font-bold pt-5">{category_name}</p>
+            <p>{availability}</p>
+        </div>
+    );
+};
+
+export default SingleJobCategory;

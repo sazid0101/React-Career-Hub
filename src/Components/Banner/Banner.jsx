b7241ce22546794243a4b2e98e098211adb6fe72
sazid0101 <shahriarislamsazid@gmail.com>
2025-04-25 16:27:46 +0600

    banner section added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,18 @@
+import bannerImg from '../../assets/assets/images/user.png'
 const Banner = () => {
     return (
-        <div>
-            <h2>Banner..........</h2>
+        <div className ="grid grid-cols-2 bg-slate-100 mt-5 mb-10 rounded-2xl">
+            <div className ="col-span-1 pl-20 pt-20">
+                <h2 className='text-5xl font-bold'>One Step <br/> Closer To Your <br/> <span className='text-teal-700'>Dream Job</span></h2>
+
+                <p className='pt-5 pb-5'>Explore thousands of job opportunities with all the ,<br /> information you need. Its your future. Come find it. Manage all <br /> your job application from start to finish.</p>
+
+                <button className="btn btn-outline btn-success">Get Started</button>
+            </div>
+
+            <div>
+                <img src={bannerImg} alt=""  />
+            </div>
         </div>
     );
 };
--- src/Components/SingleJobCategory/SingleJobCategory.jsx
+++ src/Components/SingleJobCategory/SingleJobCategory.jsx
@@ -3,7 +3,7 @@ const SingleJobCategory = ({ job }) => {
     return (
         <div className="ml-10 p-5 bg-teal-400 rounded-lg text-white">
             <div className="flex justify-center">
-                <img src={logo} alt="" srcset="" />
+                <img src={logo} alt="" />
             </div>
             <p className="font-bold pt-5">{category_name}</p>
             <p>{availability}</p>

7454d9ca95b0a4ae20e7e0e6a10a6391189dea70
Shahriar Islam <shahriarislamsazid@gmail.com>
2025-05-10 16:16:28 +0600

    ..

--- src/Components/JobCategory/JobCategory.jsx
+++ src/Components/JobCategory/JobCategory.jsx
@@ -5,10 +5,12 @@ const JobCategory = () => {
     const [jobsCategory,setJobsCategory] = useState([])
 
     useEffect(()=>{
+
         fetch('categories.json')
         .then(Response => Response.json())
         .then(data => setJobsCategory(data))
     },[])
+    
     return (
         <div>
             <h2 className="text-4xl font-bold text-center pb-5">Job Category List</h2>
